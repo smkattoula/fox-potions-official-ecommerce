e2412f8a4c20214fa03e464c36ae8f811dbe6c4c
Shaker Kattoula <smkattoula@gmail.com>
2021-08-07 16:15:33 -0700

    add product name and description to Product component

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -6,8 +6,9 @@ const Product = ({ product }) => {
     <Card className="mt-5" style={{ width: "18rem" }}>
       <Card.Img variant="top" src={product.image} className="h-100" />
       <Card.Body>
-        <Card.Title>Shirt</Card.Title>
-        <Card.Text>Cool shirt description</Card.Text>
+        <Card.Title>{product.name}</Card.Title>
+        <Card.Text>{product.description}</Card.Text>
+        <Card.Text>Price: ${product.price}</Card.Text>
         <Button variant="primary">More Details</Button>
       </Card.Body>
     </Card>
