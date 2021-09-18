86764ee351af01a2689f96e8b91e4b637cfdf981
Shaker Kattoula <smkattoula@gmail.com>
2021-09-17 17:23:23 -0700

    install react-router-dom, route screens in App.js, create ProductScreen component and display product details in the UI

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,24 @@
 import React from "react";
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
 
 import { Container } from "react-bootstrap";
 
 function App() {
   return (
-    <>
-      <Router>
-        <Header />
-        <main className="py-3">
-          <Container>
-            <HomeScreen />
-          </Container>
-        </main>
-        <Footer />
-      </Router>
-    </>
+    <Router>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <Route exact path="/" component={HomeScreen} />
+          <Route path="/product/:id" component={ProductScreen} />
+        </Container>
+      </main>
+      <Footer />
+    </Router>
   );
 }
 
--- frontend/src/products.js
+++ frontend/src/products.js
@@ -1,7 +1,7 @@
 const products = [
   {
     _id: "1",
-    name: "Airpods Wireless Bluetooth Headphones",
+    name: "Stop Talking",
     image: "/images/stoptalking.jpg",
     description:
       "Bluetooth technology lets you connect it with compatible devices wirelessly High-quality AAC audio offers immersive listening experience Built-in microphone allows you to take calls while working",
@@ -14,7 +14,7 @@ const products = [
   },
   {
     _id: "2",
-    name: "iPhone 11 Pro 256GB Memory",
+    name: "Ego Death",
     image: "/images/egodeath.jpg",
     description:
       "Introducing the iPhone 11 Pro. A transformative triple-camera system that adds tons of capability without complexity. An unprecedented leap in battery life",
@@ -27,7 +27,7 @@ const products = [
   },
   {
     _id: "3",
-    name: "Cannon EOS 80D DSLR Camera",
+    name: "Pineapple Head",
     image: "/images/pineapple.jpg",
     description:
       "Characterized by versatile imaging specs, the Canon EOS 80D further clarifies itself using a pair of robust focusing systems and an intuitive design",
@@ -40,7 +40,7 @@ const products = [
   },
   {
     _id: "4",
-    name: "Sony Playstation 4 Pro White Version",
+    name: "Matrix",
     image: "/images/matrix.jpg",
     description:
       "The ultimate home entertainment center starts with PlayStation. Whether you are into gaming, HD movies, television, music",
@@ -53,7 +53,7 @@ const products = [
   },
   {
     _id: "5",
-    name: "Logitech G-Series Gaming Mouse",
+    name: "Space Dust",
     image: "/images/spacedust.jpg",
     description:
       "Get a better handle on your games with this Logitech LIGHTSYNC gaming mouse. The six programmable buttons allow customization for a smooth playing experience",
@@ -66,7 +66,7 @@ const products = [
   },
   {
     _id: "6",
-    name: "Amazon Echo Dot 3rd Generation",
+    name: "No Planet B",
     image: "/images/noplanetb.jpg",
     description:
       "Meet Echo Dot - Our most popular smart speaker with a fabric design. It is our most compact smart speaker that fits perfectly into small space",
